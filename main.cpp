24c8f56328218ece0a9ee749263ba56f7fed89c4
MinhTung263 <49578662+MinhTung263@users.noreply.github.com>
2020-09-23 16:41:07 +0700

    1

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+class ToanHoc{
+	private:
+		int n,f[100];
+		
+	public :
+	void Nhap(){
+		cout<<"Nhap vao so phan tu trong mang: ";
+		cin>>n;
+	}
+	void Fibonaci(){
+		f[0]=1;
+		f[1]=1;
+		for(int i=2;i<n;i++){
+		f[i]=f[i-1]+f[i-2];
+		}
+		cout<<"Day fibo nho hon "<<n<<" la: ";
+		for(int i=0;i<n;i++){
+			cout<<f[i]<<"\t";
+		}
+		
+	}
+	void GiaiThua(){
+		int tam=1;
+		for(int i=1;i<=n;i++){
+			tam=tam*i;
+			
+		}
+		cout<<"\nGia thua cua "<<n<<" la "<<tam;
+	}
+};
+int main(){
+	ToanHoc toanhoc;
+	toanhoc.Nhap();
+	toanhoc.Fibonaci();
+	toanhoc.GiaiThua();
+	
+}
